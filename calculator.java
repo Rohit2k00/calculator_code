b4ad69238e1b18e05f94d6e0565e862f52aece96
ninja <rwasnik1112@gmail.com>
2023-12-22 13:28:19 +0530

    this is the updated java file which contain test cases

--- calculator.java
+++ calculator.java
@@ -45,3 +45,39 @@ public class Calculator {
         scanner.close();
     }
 }
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(15, calculator.multiply(3, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0));
+    }
+}
